f9cfa1dde0677369fad2497650d23ba201d7808a
StavrosManiatogiannis <168526227+StavrosManiatogiannis@users.noreply.github.com>
2024-07-30 23:04:44 +0300

    The main function of the bot

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+token = os.getenv("bot-token")
+
+client = commands.Bot(command_prefix= "$", intents = discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("UDP is online.")
+
+
+async def load():
+    await client.load("cogs.unix_commands")
+
+async def main():
+    async with client:
+        await load()
+        await client.start(token)
+
+
+asyncio.run(main())
